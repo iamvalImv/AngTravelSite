2bc98a1cb4cd19166a31a56c575db9f883ff8732
Valentin Immler-Valchev <vvvalvalchev@gmai.com>
2021-03-27 16:36:41 +0100

    get Sum () func and discount () func

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -4,20 +4,37 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CartService {
-  vacations:any[]=[];
-  
+  vacations: any[] = [];
+
   constructor() { }
-  
-  addToCart(product:any){
+
+  addToCart(product: any) {
     this.vacations.push(product);
   }
 
-  getVacations(){
+  getVacations() {
     return this.vacations;
   }
-  clearCart(){
-    this.vacations=[];
+  clearCart() {
+    this.vacations = [];
     return this.vacations;
+  }
+
+  getSum() {
+    let sum = 0;
+
+    //Step 1
+    this.vacations.forEach(a => sum += a.price)
+
+    //Step 2
+    if (sum > 500) {
+      return sum - ((sum / 100) * 20);
+    }
+    else if (sum > 200) {
+      return sum - ((sum / 100) * 10);
+    }
+    else
+      return sum;
   }
 }
 
